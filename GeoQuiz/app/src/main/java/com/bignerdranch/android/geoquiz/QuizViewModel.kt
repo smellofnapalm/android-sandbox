763068a6d64e175650d0044f789092b02a51fd66
smellofnapalm <69933358+smellofnapalm@users.noreply.github.com>
2022-08-06 22:27:57 +0300

    Correct work with SavedStateHandler

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val IS_ANSWERED_KEY = "IS_ANSWERED_KEY"
+const val IS_CORRECT_ANSWERED_KEY = "IS_CORRECT_ANSWERED_KEY"
 
-class QuizViewModel : ViewModel() {
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -16,21 +20,27 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
-    private var currentIndex = 0
+    private var currentIndex: Int
+        get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
+        set(value) { savedStateHandle[CURRENT_INDEX_KEY] = value }
 
     private val isAnswered = MutableList(questionBank.size) {false}
+        get() = savedStateHandle[IS_ANSWERED_KEY] ?: field
     private val isCorrectAnswered = MutableList(questionBank.size) {false}
+        get() = savedStateHandle[IS_CORRECT_ANSWERED_KEY] ?: field
 
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
     val currentQuestionText: Int
         get() = questionBank[currentIndex].textResId
+
     fun moveToNext() {
         currentIndex = (currentIndex + 1) % questionBank.size
     }
     fun moveBack() {
         currentIndex = (currentIndex + questionBank.size - 1) % questionBank.size
     }
+
     val allAnswered: Boolean
         get() = isAnswered.all { it }
     val countCorrectAnswers: Int
@@ -39,9 +49,15 @@ class QuizViewModel : ViewModel() {
         get() = questionBank.size
     var isAnsweredThis: Boolean
         get() = isAnswered[currentIndex]
-        set(value) { isAnswered[currentIndex] = value }
+        set(value) {
+            isAnswered[currentIndex] = value
+            savedStateHandle[IS_ANSWERED_KEY] = isAnswered
+        }
     var isCorrectAnsweredThis: Boolean
         get() = isCorrectAnswered[currentIndex]
-        set(value) { isCorrectAnswered[currentIndex] = value }
+        set(value) {
+            isCorrectAnswered[currentIndex] = value
+            savedStateHandle[IS_CORRECT_ANSWERED_KEY] = isCorrectAnswered
+        }
 
 }
